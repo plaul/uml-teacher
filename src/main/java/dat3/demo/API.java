5aa7147c1d1f5157604f797d1dbc1f8b2d88ed5b
plaul <lmor@kea.dk>
2023-04-15 18:50:26 +0200

    rate limiting

--- src/main/java/dat3/demo/API.java
+++ src/main/java/dat3/demo/API.java
@@ -1,12 +1,17 @@
 package dat3.demo;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.time.Duration;
 
 @Getter
 @Setter
@@ -27,18 +32,30 @@ class AiResponse {
 public class API {
   ApiService apiService;
 
+  private final Bucket bucket;
+
   public API(ApiService apiService) {
     this.apiService = apiService;
+    Bandwidth limit = Bandwidth.classic(20, Refill.greedy(20, Duration.ofMinutes(10)));
+    this.bucket = Bucket.builder()
+            .addLimit(limit)
+            .build();
   }
 
   @GetMapping
   public AiResponse getAnswer(@RequestParam String question) throws JsonProcessingException {
+    if (!bucket.tryConsume(1)) {
+      throw new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS,"To many requests, try again latter");
+    }
     String response = apiService.getResponse(question,200);
     return new AiResponse(question,response);
   }
 
   @GetMapping("/explanation")
   public AiResponse getExplanation(@RequestParam String plantuml) throws JsonProcessingException {
+    if (!bucket.tryConsume(1)) {
+      throw new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS,"To many requests, try again latter");
+    }
     String prompt = "explain the oo concepts, and explain the types of relations involved (inheritance, composition or aggregation) in this plantuml diagram.";
     prompt += "Dont use any plantuml terms in the description, but explain it from what a user will see. Provide the response nicely formatted as HTML ready to be inserted in a div:";
     String response = apiService.getResponse(prompt+plantuml,300);
@@ -47,7 +64,9 @@ public class API {
 
   @GetMapping("/uml")
   public AiResponse getUml(@RequestParam String question) throws JsonProcessingException {
-
+    if (!bucket.tryConsume(1)) {
+      throw new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS,"To many requests, try again latter");
+    }
     if(!question.toLowerCase().contains("class")){
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Text must contain at least one occurence of the word 'class'");
     }
