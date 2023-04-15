8f6e4b3cfa6c63d9173a0c126573ac0d789bb10a
plaul <lmor@kea.dk>
2023-04-15 20:26:25 +0200

    I feel lucky

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -2,8 +2,26 @@
 
 //const URL = "http://localhost:8080";
 const URL = "http://20.101.34.0:9999";
-let plantUML =""
-let plantUML2 =""
+let plantUML = ""
+let plantUML2 = ""
+
+const prompts = [
+    "Generate a class diagram for a car rental system that includes classes like Car, Rental, and Customer. A rental is associated with one car and one customer. A customer can rent multiple cars, and a car can be rented by multiple customers.\n",
+    "Create a class diagram for a chat application that has classes like User, Conversation, and Message. A user can participate in multiple conversations, and a conversation can have multiple users. A message is associated with a single conversation.\n",
+    "Generate a class diagram for an online store that includes classes like Product, Cart, and Order. A cart can contain multiple products, and an order is associated with one cart. A product can be included in multiple carts, and an order can contain multiple products.\n",
+    "Create a class diagram for a music streaming service that has classes like User, Song, and Playlist. A user can create multiple playlists and add multiple songs to a playlist. A song can be included in multiple playlists.\n",
+    "Generate a class diagram for a hotel reservation system that includes classes like Room, Reservation, and Guest. A reservation is associated with one room and one guest. A guest can make multiple reservations, and a room can be reserved by multiple guests.\n",
+    "Generate a class diagram for a library system that includes classes like Book, Author, and Borrower. A book can have multiple authors, and a borrower can borrow multiple books. A book can only be borrowed by one borrower at a time.\n",
+    "Create a class diagram for a job application system that has classes like Job, Applicant, and Recruiter. A job can have multiple applicants, and an applicant can apply to multiple jobs. A recruiter can review multiple applications.\n",
+    "Generate a class diagram for an airline reservation system that includes classes like Flight, Passenger, and Reservation. A flight can have multiple passengers, and a passenger can book multiple flights. A reservation is associated with one passenger and one flight.\n",
+    "Create a class diagram for a school enrollment system that has classes like Student, Course, and Teacher. A student can enroll in multiple courses, and a course can have multiple students. A teacher can teach multiple courses.\n",
+    "Generate a class diagram for a payroll system that includes classes like Employee, Department, and Salary. An employee can belong to one department, and a department can have multiple employees. An employee can have one salary, and a salary is associated with one employee.\n",
+    "Create a class diagram for a game engine that includes classes like Game and Renderer. The Renderer class inherits from the Game class, and the Game class has a composition relationship with the Renderer class.\n",
+    "Generate a class diagram for an e-commerce website that includes classes like Product and Category. The Category class has an aggregation relationship with the Product class, where multiple products can belong to the same category.\n",
+    "Create a class diagram for a music player that has classes like Song and Playlist. The Playlist class has a composition relationship with the Song class, where multiple songs can belong to the same playlist.\n",
+    "Generate a class diagram for a restaurant management system that includes classes like Restaurant and Menu. The Menu class has an aggregation relationship with the Restaurant class, where the menu is associated with one restaurant.\n",
+    "Create a class diagram for a company that has classes like Employee and Manager. The Manager class inherits from the Employee class, where a manager is a specific type of employee.\n"
+]
 
 
 const plantUmlArchitecture = `
@@ -24,15 +42,24 @@ S --> U : Pass new result back to user
 @enduml
 `
 const encodedSequenceDiagram = plantumlEncoder.encode(plantUmlArchitecture);
-document.getElementById("modal-image").setAttribute("src","http://www.plantuml.com/plantuml/img/"+encodedSequenceDiagram)
+document.getElementById("modal-image").setAttribute("src", "http://www.plantuml.com/plantuml/img/" + encodedSequenceDiagram)
 
 //document.getElementById("btn").onclick = getAnswer
 document.getElementById("btn-uml").onclick = getUml
 document.getElementById("btn-explain").onclick = getExplanation
-document.getElementById("btn-show-plantuml").onclick = ()=> console.log(plantUML)
+document.getElementById("btn-show-plantuml").onclick = () => console.log(plantUML)
+document.getElementById("btn-lucky").onclick = getRandomPrompt
+document.getElementById("btn-clear").onclick = ()=> {
+    document.getElementById("prompt-uml").value = ""
+    zeroState()
+}
 
+function getRandomPrompt() {
+    const randomIndex = Math.floor(Math.random() * prompts.length);
+    document.getElementById("prompt-uml").value  = prompts[randomIndex]
+}
 
-function zeroState(){
+function zeroState() {
     document.getElementById("error-uml").innerText = "";
     document.getElementById("image-uml").style.display = "none";
     document.getElementById("get-uml-spinner").style.display = "none";
@@ -50,27 +77,27 @@ async function handleHttpErrors(res) {
     return res.json()
 }
 
-async function getExplanation(){
-    if(plantUML === "") return;
+async function getExplanation() {
+    if (plantUML === "") return;
     // document.getElementById("error-explain").innerText = "";
     document.getElementById("get-explain-spinner").style.display = "inline-block";
 
     try {
         const encoded = btoa(plantUML)
-        const res = await fetch(URL+"/api/ai/explanation?plantuml="+encoded) .then(handleHttpErrors)
-        document.getElementById("explanation").innerHTML= res.answer
+        const res = await fetch(URL + "/api/ai/explanation?plantuml=" + encoded).then(handleHttpErrors)
+        document.getElementById("explanation").innerHTML = res.answer
         document.getElementById("card-explanation").style.display = "block";
 
 
-    } catch (error){
+    } catch (error) {
         document.getElementById("error-explain").innerText = error.message;
-        document.getElementById("image-explain").setAttribute("src","#")
+        document.getElementById("image-explain").setAttribute("src", "#")
     } finally {
         document.getElementById("get-explain-spinner").style.display = "none";
     }
 }
 
-async function getUml(){
+async function getUml() {
     zeroState()
     document.getElementById("error-uml").innerText = "";
     document.getElementById("image-uml").style.display = "none";
@@ -78,7 +105,7 @@ async function getUml(){
     plantUML = ""
     const question = document.getElementById("prompt-uml").value;
     try {
-        const res = await fetch(URL+"/api/ai/uml?question=" + question).then(handleHttpErrors)
+        const res = await fetch(URL + "/api/ai/uml?question=" + question).then(handleHttpErrors)
         const plantuml = plantumlEncoder.encode(res.answer);
         plantUML = res.answer
         plantUML2 = plantuml
@@ -86,10 +113,11 @@ async function getUml(){
         document.getElementById("image-uml").style.display = "block";
         document.getElementById("btn-explain").style.display = "block";
         document.getElementById("btn-show-plantuml").style.display = "block";
-    } catch (error){
+    } catch (error) {
         document.getElementById("error-uml").innerText = error.message;
-        document.getElementById("image-uml").setAttribute("src","#")
+        document.getElementById("image-uml").setAttribute("src", "#")
     } finally {
         document.getElementById("get-uml-spinner").style.display = "none";
     }
+
 }
